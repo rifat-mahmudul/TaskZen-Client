6f5e0002a69212a936d23f35ecf2f83a8999b755
rifat-mahmudul <mahmudulhasanrifat220@gmail.com>
2024-12-30 20:23:03 +0600

    secure firebase credential

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBMJZe3WMn1MChmR6k8U2SfuV6v3-ybDjI",
-    authDomain: "jobify-b1709.firebaseapp.com",
-    projectId: "jobify-b1709",
-    storageBucket: "jobify-b1709.firebasestorage.app",
-    messagingSenderId: "737177882546",
-    appId: "1:737177882546:web:07e11028e3c79769315eab"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
