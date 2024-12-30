5bcce96038d0b2fdcddc93b7c61c20a76f753e09
rifat-mahmudul <mahmudulhasanrifat220@gmail.com>
2024-12-30 19:38:44 +0600

    google signIn done

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,12 +1,25 @@
-import { createContext } from "react"
+import { createContext, useState } from "react"
 import propTypes from 'prop-types';
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../firebase/firebase.config";
 
 export const AuthContext = createContext();
 
 const AuthProvider = ({children}) => {
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const googleProvider = new GoogleAuthProvider();
+
+    const googleSignIn = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    }
     
     const authInfo = {
-        
+        user,
+        loading,
+        googleSignIn
     }
 
     return (
