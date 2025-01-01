414535b36b13769e138fa33e4314844074f3c93b
rifat-mahmudul <mahmudulhasanrifat220@gmail.com>
2025-01-01 11:32:10 +0600

    set up tanstack query, HelmetPrvider, AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,25 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from "react-router";
 import AppRoutes from './Routes/Routes';
+import AuthProvider from './provider/AuthProvider';
+import { HelmetProvider } from 'react-helmet-async';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <AppRoutes></AppRoutes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <HelmetProvider>
+        <AuthProvider>
+          <BrowserRouter>
+            <AppRoutes></AppRoutes>
+          </BrowserRouter>
+        </AuthProvider>
+      </HelmetProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
