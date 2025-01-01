c4a68fa3176bbd07403713cc629830d9ead600c1
rifat-mahmudul <mahmudulhasanrifat220@gmail.com>
2025-01-01 15:32:16 +0600

    Responsive Navbar Done

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,6 +1,8 @@
 import {Link, NavLink} from 'react-router';
 import NavItem from './NavItem';
 import { useState } from 'react';
+import { RiMenu3Fill } from "react-icons/ri";
+import { RxCross2 } from "react-icons/rx";
 
 const Navbar = () => {
 
@@ -23,7 +25,7 @@ const Navbar = () => {
                     </div>
                 </NavLink>
 
-                <div>
+                <div className='hidden lg:block'>
                     <nav>
                         <ul className='flex items-center space-x-4 font-semibold text-gray-600'>
                             {navItems}
@@ -31,7 +33,7 @@ const Navbar = () => {
                     </nav>
                 </div>
 
-                <div className='flex space-x-4'>
+                <div className='hidden lg:flex space-x-4'>
                     <Link to='/signIn'>
                         <button className='border-2 font-semibold text-gray-600 hover:bg-emerald-500 hover:text-white border-emerald-500 py-2 px-5 rounded-lg transition'>Sign In</button>
                     </Link>
@@ -40,6 +42,38 @@ const Navbar = () => {
                         <button className='font-semibold bg-emerald-500 text-white py-2 px-5 rounded-lg transition hover:bg-emerald-600'>Sign Up</button>   
                     </Link>
                 </div>
+
+                <div className='lg:hidden'>
+                    <button className='text-2xl font-bold' onClick={() => setIsOpen(!open)}>
+                        { open ?
+                            <RxCross2 />
+                            :
+                            <RiMenu3Fill/>
+                        }
+                    </button>
+                </div>
+
+                { open && 
+                    <div className='lg:hidden absolute top-14 right-0 bg-white shadow-lg p-4 rounded-lg w-3/4 bg-gradient-to-t from-emerald-500 via-white to-white ease-in-out duration-1000'>
+                        <div>
+                            <nav>
+                                <ul className='flex flex-col text-center space-y-4 font-semibold text-gray-600'>
+                                {navItems}
+                                </ul>
+                            </nav>
+                        </div>
+
+                        <div className='flex flex-col text-center space-y-4 mt-4'>
+                            <Link to='/signIn'>
+                                <button className='border-2 font-semibold text-gray-600 hover:bg-emerald-500 hover:text-white border-emerald-500 py-2 px-5 rounded-lg transition'>Sign In</button>
+                            </Link>
+                            
+                            <Link to='/signUp'>
+                                <button className='font-semibold bg-emerald-500 text-white py-2 px-5 rounded-lg transition hover:bg-emerald-600'>Sign Up</button>   
+                            </Link>
+                        </div>
+                    </div>
+                }
             </div>
 
         </header>
