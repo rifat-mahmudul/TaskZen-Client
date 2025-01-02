54ac125bcdfa90780eedbb0b3bc6006fb26a8e52
rifat-mahmudul <mahmudulhasanrifat220@gmail.com>
2025-01-02 11:40:29 +0600

    Applicant page design done

--- src/pages/Authentication/SignUp/ApplicantSignUp.jsx
+++ src/pages/Authentication/SignUp/ApplicantSignUp.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router"
+import { FcGoogle } from "react-icons/fc";
 
 const ApplicantSignUp = () => {
     return (
@@ -11,6 +13,15 @@ const ApplicantSignUp = () => {
                     className="w-full p-3 outline-none rounded-lg border-2 border-green-700" 
                     type="text" 
                     placeholder="Enter Your Name"
+                    />
+                </div>
+
+                {/* name */}
+                <div className="mt-4">
+                    <h1 className="font-semibold text-xl text-white mb-2">Upload Photo</h1>
+                    <input 
+                    className="w-full p-3 outline-none rounded-lg border-2 border-green-700" 
+                    type="file" 
                     />
                 </div>
 
@@ -36,11 +47,32 @@ const ApplicantSignUp = () => {
 
                 <button 
                 type="submit"
-                className="mt-4 py-3 px-5 w-full text-center  rounded-lg bg-emerald-700 text-xl text-white font-semibold">
+                className="mt-6 py-3 px-5 w-full text-center  rounded-lg bg-emerald-700 text-xl text-white font-semibold">
                     Sign Up as an Applicant
                 </button>
 
+                <div>
+                    <p className="mt-2 text-white">
+                        <span>Already have an account ? please </span>
+                        <Link to='/signIn'><span className="text-emerald-900 underline">Sign in</span></Link>
+                    </p>
+                </div>
+
             </form>
+
+            
+            <div className='flex items-center justify-center space-x-4 mt-5'>
+                <div className='border border-gray-300 w-full'></div>
+                    <div>
+                        <h1>Or</h1>
+                    </div>
+                <div className='border border-gray-300 w-full'></div>
+            </div>
+
+            <button 
+                className='border-2 border-white py-3 w-full mt-5 rounded-lg font-bold flex items-center justify-center space-x-2 disabled:cursor-not-allowed'>
+                <FcGoogle className='text-3xl' /> <span className="text-white">Continue With Google</span>
+            </button>
         </div>
     )
 }
