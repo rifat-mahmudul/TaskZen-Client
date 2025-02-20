88607e1bb25a85d1ac1f0c9a114afb526ca0312b
rifat-mahmudul <mahmudulhasanrifat220@gmail.com>
2025-02-20 15:09:29 +0600

    Delete unnecessery component

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -1,21 +1,12 @@
-
 import { Link } from 'react-router';
 import img1 from '../../assets/banner1.png';
 import img2 from '../../assets/banner2.png';
-import { SiAirtable } from "react-icons/si";
-import { RxSketchLogo } from "react-icons/rx";
-import { FaSlack } from "react-icons/fa6";
-import { SiLivechat } from "react-icons/si";
-import { FaGitlab } from "react-icons/fa6";
-import { FaLinkedin } from "react-icons/fa";
-import { FaDribbble } from "react-icons/fa6";
-import Marquee from "react-fast-marquee";
 
 const Banner = () => {
     return (
         <section className="bg-gradient-to-t from-emerald-400 via-emerald-100 to-emerald-50 py-10 sm:py-16 min-h-[calc(100vh-69.33px)] flex flex-col items-center justify-center">
         
-            <div className="max-w-[90%] xl:max-w-[1200px] mx-auto text-center sm:flex items-center justify-between sm:gap-5 lg:gap-0 border-b-2 border-white pb-16">
+            <div className="max-w-[90%] xl:max-w-[1200px] mx-auto text-center sm:flex items-center justify-between sm:gap-5 lg:gap-0">
 
                 <div>
                     <img className='mx-auto sm:mx-0 mb-5 sm:mb-0 h-40 sm:h-full animate-pulse' src={img1} alt="" />
@@ -33,94 +24,6 @@ const Banner = () => {
                     <img className='mx-auto sm:mx-0 mt-10 sm:mb-0 h-20 sm:h-full' src={img2} alt="" />
                 </div>
 
-            </div>
-
-            <div className='max-w-[90%] xl:max-w-[1200px] mx-auto '>
-                <Marquee>
-                    <div className="flex space-x-8 mt-10">
-
-                        <div className='flex space-x-2 items-center text-white text-2xl font-bold'>
-                            <div>
-                                <SiAirtable />
-                            </div>
-
-                            <div>
-                                <h1>Airtable</h1>
-                            </div>
-                        </div>
-
-                        <div className='flex space-x-2 items-center text-white text-2xl font-bold'>
-                            <div>
-                                <RxSketchLogo />
-                            </div>
-
-                            <div>
-                                <h1>Sketch</h1>
-                            </div>
-                        </div>
-
-                        <div className='flex space-x-2 items-center text-white text-2xl font-bold'>
-                            <div>
-                                <FaSlack />
-                            </div>
-
-                            <div>
-                                <h1>Slack</h1>
-                            </div>
-                        </div>
-
-                        <div className='flex space-x-2 items-center text-white text-2xl font-bold'>
-                            <div>
-                                <SiLivechat  />
-                            </div>
-
-                            <div>
-                                <h1>LiveChat</h1>
-                            </div>
-                        </div>
-
-                        <div className='flex space-x-2 items-center text-white text-2xl font-bold'>
-                            <div>
-                                <FaGitlab  />
-                            </div>
-
-                            <div>
-                                <h1>GitLab</h1>
-                            </div>
-                        </div>
-
-                        <div className='flex space-x-2 items-center text-white text-2xl font-bold'>
-                            <div>
-                                <FaLinkedin  />
-                            </div>
-
-                            <div>
-                                <h1>Linkedin</h1>
-                            </div>
-                        </div>
-
-                        <div className='flex space-x-2 items-center text-white text-2xl font-bold'>
-                            <div>
-                                <FaDribbble  />
-                            </div>
-
-                            <div>
-                                <h1>Dribbble</h1>
-                            </div>
-                        </div>
-
-                        <div className='flex space-x-2 items-center text-white text-2xl font-bold'>
-                            <div>
-                                <SiAirtable  />
-                            </div>
-
-                            <div>
-                                <h1>Airtable</h1>
-                            </div>
-                        </div>
-
-                    </div>
-                </Marquee>
             </div>
 
         </section>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,9 @@
 import Banner from "../components/Home/Banner"
-import Faq from "../components/Home/Faq"
-import JobsWork from "../components/Home/JobsWork"
 
 const Home = () => {
     return (
         <section>
             <Banner></Banner>
-            <JobsWork></JobsWork>
-            <Faq></Faq>
         </section>
     )
 }
