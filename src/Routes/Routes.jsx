9520782f70b21bc694831012edb7389a86800146
rifat-mahmudul <mahmudulhasanrifat220@gmail.com>
2025-02-20 15:19:37 +0600

    modify navbar

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,14 +3,8 @@ import Root from "../Layout/Root";
 import Home from "../pages/Home";
 import Error from "../pages/Error";
 import SignIn from "../pages/Authentication/Login/SignIn";
-import SignUp from "../pages/Authentication/SignUp/SignUp";
-import ApplicantSignUp from "../pages/Authentication/SignUp/ApplicantSignUp";
-import EmployerSignUp from "../pages/Authentication/SignUp/EmployerSignUp";
 import ApplicantSignIn from "../pages/Authentication/Login/ApplicantSignIn";
 import EmployerSignIn from "../pages/Authentication/Login/EmployerSignIn";
-import Dashboard from "../pages/Dashboard/Dashboard";
-import DashHome from "../pages/Dashboard/DashHome";
-import AllUser from "../pages/Dashboard/AllUser";
 
 const AppRoutes = () => {
     return (
@@ -27,24 +21,6 @@ const AppRoutes = () => {
                 />
                 <Route path="/signIn/applicant-signin" element={<ApplicantSignIn></ApplicantSignIn>}></Route>
                 <Route path="employer-signIn" element={<EmployerSignIn></EmployerSignIn>}></Route>
-            </Route>
-
-            <Route path="/signUp" element={<SignUp></SignUp>}>
-                <Route
-                    index
-                    element={<Navigate to="applicant-signup" replace />}
-                />
-                <Route path="/signUp/applicant-signup" element={<ApplicantSignUp></ApplicantSignUp>}></Route>
-                <Route path="employer-signup" element={<EmployerSignUp></EmployerSignUp>}></Route>
-            </Route>
-
-            <Route path="/dashboard" element={<Dashboard></Dashboard>}>
-                <Route
-                    index
-                    element={<Navigate to="dash-home" replace />}
-                />
-                <Route path="/dashboard/dash-home" element={<DashHome></DashHome>}></Route>
-                <Route path="all-user" element={<AllUser></AllUser>}></Route>
             </Route>
 
             <Route path="*" element={<Error></Error>}></Route>
--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,5 +1,4 @@
 import {Link, NavLink} from 'react-router';
-import NavItem from './NavItem';
 import { useState } from 'react';
 import { RiMenu3Fill } from "react-icons/ri";
 import { RxCross2 } from "react-icons/rx";
@@ -7,12 +6,6 @@ import { RxCross2 } from "react-icons/rx";
 const Navbar = () => {
 
     const [open, setIsOpen] = useState(false);
-
-    const navItems = <>
-        <NavItem setIsOpen={setIsOpen} address={'/'} navName={'Home'}></NavItem>
-        <NavItem setIsOpen={setIsOpen} address={'/all-jobs'} navName={'All Jobs'}></NavItem>
-        <NavItem setIsOpen={setIsOpen} address={'/dashboard'} navName={'Dashboard'}></NavItem>
-    </>
 
     return (
         <header className='sticky top-0 backdrop-blur-lg z-50 bg-emerald-50'>
@@ -25,21 +18,9 @@ const Navbar = () => {
                     </div>
                 </NavLink>
 
-                <div className='hidden lg:block'>
-                    <nav>
-                        <ul className='flex items-center space-x-4 font-semibold text-gray-600'>
-                            {navItems}
-                        </ul>
-                    </nav>
-                </div>
-
                 <div className='hidden lg:flex space-x-4'>
                     <Link to='/signIn'>
                         <button className='border-2 font-semibold text-gray-600 hover:bg-emerald-500 hover:text-white border-emerald-500 py-2 px-5 rounded-lg transition'>Sign In</button>
-                    </Link>
-                    
-                    <Link to='/signUp'>
-                        <button className='font-semibold bg-emerald-500 text-white py-2 px-5 rounded-lg transition hover:bg-emerald-600'>Sign Up</button>   
                     </Link>
                 </div>
 
@@ -66,10 +47,6 @@ const Navbar = () => {
                         <div className='flex flex-col text-center space-y-4 mt-4'>
                             <Link to='/signIn'>
                                 <button className='border-2 font-semibold text-gray-600 hover:bg-emerald-500 hover:text-white border-emerald-500 py-2 px-5 rounded-lg transition'>Sign In</button>
-                            </Link>
-                            
-                            <Link to='/signUp'>
-                                <button className='font-semibold bg-emerald-500 text-white py-2 px-5 rounded-lg transition hover:bg-emerald-600'>Sign Up</button>   
                             </Link>
                         </div>
                     </div>
