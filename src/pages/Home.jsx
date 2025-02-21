d9be8e06850027530c894c394ebecb75323d0bf4
rifat-mahmudul <mahmudulhasanrifat220@gmail.com>
2025-02-21 10:40:40 +0600

    home page title done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import Banner from "../components/Home/Banner"
+import HelmetTitle from "../components/shared/HelmetTitle"
 
 const Home = () => {
     return (
         <section>
+            <HelmetTitle title={'Home'}></HelmetTitle>
             <Banner></Banner>
         </section>
     )
